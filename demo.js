fda6794ee9ec23e13ed30a44d18b02a547675cc1
Richard Goni Patti Cruz <richardgonzalo017@gmail.com>
2022-09-04 17:13:16 -0400

    Agregando dia al archivo demo

--- demo.js
+++ demo.js
@@ -1,3 +1,4 @@
-console.log('Hola Clase');
+console.log('Este es el cambio realizado en el archivo demo.js')
 
-console.log('Diplomado Fullstatck');
+console.log("Hola clase dia 6 - Agosto 25, 2022");
+console.log("Diplomado Fullstack");
